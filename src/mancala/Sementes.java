dabe99028eac5386f9b902fdc0a6c6b1549ba584
Daniel Gonçalves <daniell5984@gmail.com>
2021-04-04 17:45:39 +0100

    Adição de classes

---
+++ src/mancala/Sementes.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mancala;
+
+/**
+ * Cada Jogador possui 24 sementes , ao todo o campo tem 48 sementes 24 para cada jogador
+ * @author DanielSilva
+ */
+public class Sementes {
+    
+}
