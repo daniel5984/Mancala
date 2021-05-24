19994a303fae8d3b701439e3513d5aab208dfc0b
Daniel Gonçalves <daniell5984@gmail.com>
2021-05-24 15:38:26 +0100

    Organização de ficheiros e criação do layout do menu Principal

--- src/controlers/FxDemoController.java
+++ src/controlers/FxDemoController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mancala;
+package controlers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,45 +24,25 @@ import javafx.scene.layout.HBox;
  */
 public class FxDemoController implements Initializable {
 
-    @FXML
     private Label label;
 
-    @FXML
-    private Button button;
 
-    @FXML
-    private Button buttonsSair;
-
-    @FXML
     private TextField caixaTexto;
 
-    @FXML
-    private Button buttonEscrever;
-    @FXML
     private Slider slider1;
-    @FXML
     private Slider slider2;
-    @FXML
     private Label label3;
 
-    @FXML
     private TextField text_input1;
-    @FXML
     private TextField text_input2;
-    @FXML
     private Label label1;
-    @FXML
     private Label label2;
     @FXML
     private HBox hBox;
-    @FXML
     private ComboBox<String> cmbBox;
-    @FXML
     private Label labelOld;
-    @FXML
     private Label labelNew;
 
-    @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -71,19 +51,17 @@ public class FxDemoController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        label1.setText("Daniel");
-        label2.setText("Bem-vindo");
-
-        label1.textProperty().bind(text_input1.textProperty());
-        label2.textProperty().bind(text_input2.textProperty().concat(", ").concat(text_input1.textProperty()));
-
-        label3.textProperty().bind(slider1.valueProperty().add(slider2.valueProperty()).asString());
-
-        
-        cmbBox.valueProperty().addListener((obs, o, n)->respostaCombo(o,n));
+//        label1.setText("Daniel");
+//        label2.setText("Bem-vindo");
+//
+//        label1.textProperty().bind(text_input1.textProperty());
+//        label2.textProperty().bind(text_input2.textProperty().concat(", ").concat(text_input1.textProperty()));
+//
+//        label3.textProperty().bind(slider1.valueProperty().add(slider2.valueProperty()).asString());
+//
+//        cmbBox.valueProperty().addListener((obs, o, n) -> respostaCombo(o, n));
     }
 
-    @FXML
     private void buttonsSair(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Sair");
@@ -96,7 +74,6 @@ public class FxDemoController implements Initializable {
     }
     
     
-    @FXML
     private void buttonEscreverOnclick(ActionEvent event) {
 
         label.setText(caixaTexto.getText());
